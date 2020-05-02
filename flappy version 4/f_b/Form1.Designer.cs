40c09001c0353809b25ac2757546500f274f0b83
Nehal Ahmed <41865951+nehal2210@users.noreply.github.com>
2020-05-02 04:11:52 -0700

    First Commit of Flappy Bird

---
+++ flappy version 4/f_b/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace f_b
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.t1 = new System.Windows.Forms.PictureBox();
+            this.t2 = new System.Windows.Forms.PictureBox();
+            this.t3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.h2 = new System.Windows.Forms.PictureBox();
+            this.h1 = new System.Windows.Forms.PictureBox();
+            this.h4 = new System.Windows.Forms.PictureBox();
+            this.h3 = new System.Windows.Forms.PictureBox();
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.gover = new System.Windows.Forms.Label();
+            this.points = new System.Windows.Forms.Label();
+            this.Start = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.high_score = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 328);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(752, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // t1
+            // 
+            this.t1.Image = ((System.Drawing.Image)(resources.GetObject("t1.Image")));
+            this.t1.Location = new System.Drawing.Point(24, 279);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(74, 50);
+            this.t1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.t1.TabIndex = 1;
+            this.t1.TabStop = false;
+            // 
+            // t2
+            // 
+            this.t2.Image = ((System.Drawing.Image)(resources.GetObject("t2.Image")));
+            this.t2.Location = new System.Drawing.Point(304, 277);
+            this.t2.Name = "t2";
+            this.t2.Size = new System.Drawing.Size(74, 50);
+            this.t2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.t2.TabIndex = 2;
+            this.t2.TabStop = false;
+            // 
+            // t3
+            // 
+            this.t3.Image = ((System.Drawing.Image)(resources.GetObject("t3.Image")));
+            this.t3.Location = new System.Drawing.Point(612, 279);
+            this.t3.Name = "t3";
+            this.t3.Size = new System.Drawing.Size(74, 50);
+            this.t3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.t3.TabIndex = 3;
+            this.t3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // h2
+            // 
+            this.h2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.h2.Image = ((System.Drawing.Image)(resources.GetObject("h2.Image")));
+            this.h2.Location = new System.Drawing.Point(132, 258);
+            this.h2.Name = "h2";
+            this.h2.Size = new System.Drawing.Size(79, 160);
+            this.h2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.h2.TabIndex = 4;
+            this.h2.TabStop = false;
+            // 
+            // h1
+            // 
+            this.h1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.h1.Image = ((System.Drawing.Image)(resources.GetObject("h1.Image")));
+            this.h1.Location = new System.Drawing.Point(132, -1);
+            this.h1.Name = "h1";
+            this.h1.Size = new System.Drawing.Size(79, 160);
+            this.h1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.h1.TabIndex = 5;
+            this.h1.TabStop = false;
+            // 
+            // h4
+            // 
+            this.h4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.h4.Image = ((System.Drawing.Image)(resources.GetObject("h4.Image")));
+            this.h4.Location = new System.Drawing.Point(516, 258);
+            this.h4.Name = "h4";
+            this.h4.Size = new System.Drawing.Size(79, 160);
+            this.h4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.h4.TabIndex = 6;
+            this.h4.TabStop = false;
+            // 
+            // h3
+            // 
+            this.h3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.h3.Image = ((System.Drawing.Image)(resources.GetObject("h3.Image")));
+            this.h3.Location = new System.Drawing.Point(516, -1);
+            this.h3.Name = "h3";
+            this.h3.Size = new System.Drawing.Size(79, 160);
+            this.h3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.h3.TabIndex = 7;
+            this.h3.TabStop = false;
+            // 
+            // bird
+            // 
+            this.bird.Image = ((System.Drawing.Image)(resources.GetObject("bird.Image")));
+            this.bird.Location = new System.Drawing.Point(42, 185);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(31, 34);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bird.TabIndex = 8;
+            this.bird.TabStop = false;
+            this.bird.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.form_keydown);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // gover
+            // 
+            this.gover.AutoSize = true;
+            this.gover.BackColor = System.Drawing.Color.Black;
+            this.gover.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gover.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.gover.Location = new System.Drawing.Point(264, 73);
+            this.gover.Name = "gover";
+            this.gover.Size = new System.Drawing.Size(221, 57);
+            this.gover.TabIndex = 9;
+            this.gover.Text = "Game Over";
+            // 
+            // points
+            // 
+            this.points.AutoSize = true;
+            this.points.BackColor = System.Drawing.Color.Black;
+            this.points.Font = new System.Drawing.Font("Monotype Corsiva", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.points.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.points.Location = new System.Drawing.Point(346, -1);
+            this.points.Name = "points";
+            this.points.Size = new System.Drawing.Size(32, 39);
+            this.points.TabIndex = 10;
+            this.points.Text = "0";
+            // 
+            // Start
+            // 
+            this.Start.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Start.Location = new System.Drawing.Point(330, 196);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 11;
+            this.Start.Text = "START";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(295, 143);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(143, 47);
+            this.axWindowsMediaPlayer1.TabIndex = 12;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // high_score
+            // 
+            this.high_score.AutoSize = true;
+            this.high_score.BackColor = System.Drawing.Color.Black;
+            this.high_score.Font = new System.Drawing.Font("Monotype Corsiva", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.high_score.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.high_score.Location = new System.Drawing.Point(1, -1);
+            this.high_score.Name = "high_score";
+            this.high_score.Size = new System.Drawing.Size(123, 24);
+            this.high_score.TabIndex = 13;
+            this.high_score.Text = "High Score : 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(700, 418);
+            this.Controls.Add(this.high_score);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.points);
+            this.Controls.Add(this.gover);
+            this.Controls.Add(this.bird);
+            this.Controls.Add(this.h3);
+            this.Controls.Add(this.h4);
+            this.Controls.Add(this.h1);
+            this.Controls.Add(this.h2);
+            this.Controls.Add(this.t3);
+            this.Controls.Add(this.t2);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox t1;
+        private System.Windows.Forms.PictureBox t2;
+        private System.Windows.Forms.PictureBox t3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox h2;
+        private System.Windows.Forms.PictureBox h1;
+        private System.Windows.Forms.PictureBox h4;
+        private System.Windows.Forms.PictureBox h3;
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label gover;
+        private System.Windows.Forms.Label points;
+        private System.Windows.Forms.Button Start;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Label high_score;
+    }
+}
+
---
+++ flappy version 4/f_b/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib; // media player library
+namespace f_b
+{
+    public partial class Form1 : Form
+    {
+        int hi_score;
+        int score = 0; // variables
+        Random r = new Random();
+
+        // medias player objects for sound
+        WindowsMediaPlayerClass play_out = new WindowsMediaPlayerClass();
+         WindowsMediaPlayerClass play_point = new WindowsMediaPlayerClass();
+         WindowsMediaPlayerClass play_wing = new WindowsMediaPlayerClass();
+        WindowsMediaPlayerClass play_die = new WindowsMediaPlayerClass();
+        WindowsMediaPlayerClass play_swooshing = new WindowsMediaPlayerClass();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            /* Buffered graphics require that the updated graphics data is first written to a buffer.
+             * The data in the graphics buffer is then quickly written to displayed surface memory.
+             * The relatively quick switch of the displayed graphics memory typically reduces the flicker that can otherwise occur.
+                
+             */
+            this.DoubleBuffered = true;
+            
+            gover.Visible = false;
+            play_point.URL = "point.wav";
+            play_out.URL = "hit.wav";
+            play_wing.URL = "wing.wav";
+            play_die.URL = "die.wav";
+            play_swooshing.URL = "swooshing.wav";
+
+            play_swooshing.stop();
+            play_wing.stop();
+            play_die.stop();
+            play_out.stop();
+            play_point.stop();
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            Start.Visible = true;
+            Start.Enabled = true;
+            bird.Enabled = false;
+
+             hi_score = Convert.ToInt32(System.IO.File.ReadAllText("highscore.txt"));
+            high_score.Text = Convert.ToString("High Score : " + hi_score.ToString());
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int size_hurdle = r.Next(0, 25);
+
+            movtree(7);
+            movhurdles(2,size_hurdle);
+            gameover();
+            
+        }
+        void movtree(int speed)
+        {
+
+            if (t1.Left >= -74)
+            {
+                t1.Left += -speed;
+                
+            }
+            else
+            {
+                t1.Left = 800;
+            }
+            if (t2.Left >= -74)
+            {
+                t2.Left += -speed;
+
+            }
+            else
+            {
+                t2.Left = 800;
+            }
+            if (t3.Left >= -74)
+            {
+                t3.Left += -speed;
+
+            }
+            else
+            {
+                t3.Left = 800;
+            }
+        }
+
+        void movhurdles(int speed, int size_hurdle)
+        {
+
+            if (h1.Left >= -79)
+            {
+                h1.Left += -speed;
+
+            }
+            else
+            {
+                h1.Left = 800;
+                //h1.Height = r.Next(100,150);
+             //   h1.Top += size_hurdle;
+
+                if (size_hurdle %2!=0)
+                {
+                    h1.Height += size_hurdle;
+
+                }
+                else
+                {
+                    h1.Height += -size_hurdle;
+
+                }
+
+            }
+
+            if (h2.Left >= -79)
+            {
+                h2.Left += -speed;
+
+            }
+            else
+            {
+                h2.Left = 800;
+                //h2.Height = r.Next(100, 156);
+                if (size_hurdle!=0)
+                {
+                    h2.Top += size_hurdle;
+                    //h2.Height += -size_hurdle;
+                }
+                else
+                {
+                    h2.Top += -size_hurdle;
+                    h2.Height += size_hurdle;
+                }
+            }
+
+            if (h3.Left >= -79)
+            {
+                h3.Left += -speed;
+
+            }
+            else
+            {
+                h3.Left = 800;
+                //  h3.Height = r.Next(100, 170);
+                if (size_hurdle %2==0)
+                {
+                    h3.Height += size_hurdle;
+
+                }
+                else
+                {
+                    h3.Height += -size_hurdle;
+
+                }
+
+            }
+
+            if (h4.Left >= -79)
+            {
+                h4.Left += -speed;
+                
+
+            }
+            else
+            {
+                // h4.Height = r.Next(100, 170);
+                h4.Left = 800;
+                if (size_hurdle%2==0)
+                {
+                    h4.Top += size_hurdle;
+                   // h4.Height += -size_hurdle;
+                }
+                else
+                {
+                    h4.Top += -size_hurdle;
+                    h4.Height += size_hurdle;
+
+                }
+
+            }
+
+        }
+
+
+
+
+
+
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (bird.Top <= 296)
+            {
+                bird.Top += 1;
+                play_swooshing.play();
+            }
+
+          
+            if ((h1.Location.X == 0 && h2.Location.X == 0) || (h3.Location.X  == 0 && h4.Location.X == 0))
+            {
+                score += 1;
+                points.Text = score.ToString();
+                play_point.play();
+
+
+            }
+        }
+
+                   private void form_keydown(object sender, PreviewKeyDownEventArgs e)
+                    {
+
+                    }
+                    void gameover()
+                    {
+                        if (bird.Bounds.IntersectsWith(h1.Bounds)||(bird.Bounds.IntersectsWith(h2.Bounds))||(bird.Bounds.IntersectsWith(h3.Bounds))||(bird.Bounds.IntersectsWith(h4.Bounds)) ||bird.Top==296)
+                        {
+                             play_out.play();
+                             play_die.play();
+
+                            timer1.Enabled = false;
+                            timer2.Enabled = false;
+                            gover.Visible = true;
+                            Start.Visible = true;
+                            Start.Enabled = true;
+          
+                // bird.Enabled = false;// kusch nhi ho rha
+                if (score > hi_score)
+                {
+                    System.IO.File.WriteAllText("highscore.txt",score.ToString());
+
+                }
+
+            }
+        }
+                    private void Form1_KeyDown(object sender, KeyEventArgs e)
+                    {
+            if (timer1.Enabled==true)
+            {
+                if (e.KeyCode == Keys.Up)
+                {
+                    bird.Top += -40;
+                    play_wing.play();
+                }
+
+            }
+
+
+        }
+
+                   
+
+
+
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            
+            if (gover.Visible==true)
+            {
+                Application.Restart();
+
+            }
+            else
+            {
+                timer1.Enabled = true;
+                timer2.Enabled = true;
+                Start.Visible = false;
+                Start.Enabled = false;
+                bird.Enabled = true;
+               
+
+            }
+        }
+
+
+
+    }
+            }
